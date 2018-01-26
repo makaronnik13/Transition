f474df107452e4db6fdec6729be589a717eff400
Victor Dorofeyev <vinvirinvi@gmail.com>
2018-01-27 00:16:07 +0300

    Model classes

---
+++ Assets/Scripts/Symbol.cs
@@ -0,0 +1,9 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Symbol : ScriptableObject
+{
+    [InlineEditor(InlineEditorModes.LargePreview)]
+    public Sprite image;
+}
---
+++ Assets/Scripts/Transmission.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Transmission : ScriptableObject
+{
+    public Symbol[] content;
+    Choice[] choices;
+}

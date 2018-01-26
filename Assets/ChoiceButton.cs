03ce4d80306bec8f2c28027a9d2071d9914424fa
Nikita <biggreenorange@gmail.com>
2018-01-27 02:21:51 +0300

    choice visual

--- Assets/ChoiceButton.cs
+++ Assets/ChoiceButton.cs
@@ -1,11 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChoiceButton : MonoBehaviour {
 
-	public void Init()
+	public Transform symbolsPanel;
+	public Text textPanel;
+
+	public void Init(Choice choise)
 	{
-		//GetComponent<>
+		if (choise.symbolContent.Length > 0) {
+			Destroy (textPanel.gameObject);
+			foreach(Symbol s in choise.symbolContent)
+			{
+				GameObject symbol = new GameObject ();
+				symbol.transform.SetParent (symbolsPanel);
+				symbol.AddComponent<Image> ().sprite = s.image;
+			}
+		} else 
+		{
+			Destroy (symbolsPanel.gameObject);
+			textPanel.text = choise.textContent;
+		}
+
+		GetComponent<Button> ().onClick.AddListener (()=>
+		{
+				//TransitionsManager.Instance.	
+		});
 	}
 }
--- Assets/Scripts/Controller/TransitionsManager.cs
+++ Assets/Scripts/Controller/TransitionsManager.cs
@@ -1,16 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HoloToolkit.Unity;
 
-public class TransitionsManager : MonoBehaviour {
+public class TransitionsManager : Singleton<TransitionsManager> 
+{
+	//public Action<>
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }

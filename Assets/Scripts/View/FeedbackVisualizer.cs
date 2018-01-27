1ff36ba27724efac400796789a39515aa9ae7b53
Nikita <biggreenorange@gmail.com>
2018-01-27 05:57:39 +0300

    Feedback visualiser

---
+++ Assets/Scripts/View/FeedbackVisualizer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FeedbackVisualizer : MonoBehaviour {
+
+	public Text text;
+	private Button button;
+
+	// Use this for initialization
+	void Start () 
+	{
+		button = GetComponent<Button> ();
+		button.onClick.AddListener (()=>{
+			TransmissionManager.Instance.CloseTransmission();
+			button.enabled = false;
+			text.enabled = false;
+			GetComponent<Image> ().enabled = false;
+		});
+		button.enabled = false;
+		text.enabled = false;
+		GetComponent<Image> ().enabled = false;
+		TransmissionManager.Instance.OnChoiceApplied += ShowFeedback;
+	}
+
+	private void ShowFeedback(Choice choice)
+	{
+		if(choice.textContent!="")
+		{
+			text.text = choice.textFeedback;
+			button.enabled = true;
+			text.enabled = true;
+			GetComponent<Image> ().enabled = true;
+		}
+	}
+}

3ad29247b5b1162d4bd2bc29c4f645cf2f4e366f
Victor Dorofeyev <vinvirinvi@gmail.com>
2018-01-27 05:24:15 +0300

    Symbol Visualizer

--- Assets/Scripts/View/SymbolsVisualizer.cs
+++ Assets/Scripts/View/SymbolsVisualizer.cs
@@ -1,8 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SymbolsVisualizer : MonoBehaviour
 {
-    
+    //public Transmission testTransmission;
+
+    private void Start()
+    {
+        TransmissionManager.Instance.OnTransmissionRecieved += DrawTransmission;
+        TransmissionManager.Instance.OnTransmissionClosed += Hide;
+    }
+
+    //[Button]
+    //private void TestDraw ()
+    //{
+    //    DrawTransmission(testTransmission);
+    //}
+
+    private void DrawTransmission (Transmission transmission)
+    {
+        foreach (Symbol symbol in transmission.content)
+        {
+            GameObject symbolGObj = new GameObject();
+			symbolGObj.transform.SetParent(transform, false);
+			symbolGObj.transform.localScale = Vector3.one;
+			symbolGObj.AddComponent<Image>();
+			symbolGObj.GetComponent<Image>().sprite = symbol.image;
+        }
+    }
+
+    private void Hide (Transmission transmission, Choice choice)
+    {
+        foreach (Transform symbolChild in transform)
+        {
+            Destroy(symbolChild);
+        }
+    }
 }

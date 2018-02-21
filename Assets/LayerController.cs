decce246f5dc43802d4b4ee5760e0838d87a7e0f
Nikita <biggreenorange@gmail.com>
2018-02-21 21:10:34 +0300

    Layer sorting

---
+++ Assets/LayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class LayerController : MonoBehaviour {
+
+    public bool update = false;
+    private SpriteRenderer SRenderer;
+
+    private void Awake()
+    {
+        SRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        SetLayer();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (update)
+        {
+            SetLayer();
+        }
+	}
+
+    void SetLayer()
+    {
+        SRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y*10);
+    }
+}
